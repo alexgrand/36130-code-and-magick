3228924872b66ec168f40d501edce62e431d519b
Alexrand <namelost@yandex.ru>
2018-01-29 19:01:37 +0800

    added: текст сообщения на облаке.

--- js/stat.js
+++ js/stat.js
@@ -4,14 +4,26 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_SHADOW_COL = 'rgba(0, 0, 0, 0.7)';
 var GAP = 10;
-var CTX_FONT = '16px PT Mono';
+var FONT_SIZE = 16;
+var FONT_FAMILY = 'PT Mono';
+var HISTOGRAM_HEIGHT = 150;
 
 var renderCloud = function (ctx, x, y, color = "#fff") {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 }
 
+var renderText = function (ctx, x, y, text, color = '#000') {
+  ctx.fillStyle = color;
+  ctx.font = FONT_SIZE + 'px ' + FONT_FAMILY;
+  ctx.fillText(text, x, y);
+  console.log(ctx);
+}
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_SHADOW_COL);
    renderCloud(ctx, CLOUD_X, CLOUD_Y);
+
+   renderText(ctx, CLOUD_X + 2*GAP, CLOUD_Y + 3*GAP, 'Ура вы победили!');
+   renderText(ctx, CLOUD_X + 2*GAP, CLOUD_Y + 3*GAP + FONT_SIZE, 'Список результатов:')
 }
