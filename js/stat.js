ae61699f7bfaa3212860c2036e19d5df69bb13a5
Alexrand <namelost@yandex.ru>
2018-01-30 00:39:38 +0800

    added: функции выбора цвета, определения высоты колонки.

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,4 @@
+'use strict';
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_WIDTH = 420;
@@ -9,14 +10,14 @@ var FONT_FAMILY = 'PT Mono';
 var HISTM_HEIGHT = 150;
 var COL_WIDTH = 40;
 var COL_DIST = 50;
-var PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
 
-var renderCloud = function (ctx, x, y, color = "#fff") {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var renderText = function (ctx, x, y, text, color = '#000') {
+var renderText = function (ctx, x, y, text) {
+  var color = '#000';
   ctx.fillStyle = color;
   ctx.font = FONT_SIZE + 'px ' + FONT_FAMILY;
   ctx.fillText(text, x, y);
@@ -33,38 +34,40 @@ var renderCol = function (ctx, x, colHeight, player, color) {
 
 var renderAllCols = function (ctx, names, times) {
   var totalPlayers = names.length;
-  var bestPlayer = getBiggestScore(names, times);
   for (var i = 0; i < totalPlayers; i++) {
-    var colColor = 'rgba(255, 0, 0, 1)';
-    var colHeight = HISTM_HEIGHT;
-    if (names[i] === 'Вы') {
+    var currentScore = Math.floor(times[i]);
+    var currentPlayer = names[i];
+    var colColor = getColColor(currentPlayer);
+    var colHeight = getColHeight(times, currentScore);
 
-    } else {
-      colColor = 'rgba(0, 0, 255, ' + Math.random() + ')';
-      colHeight = 100;
-    }
     renderCol(ctx, COL_WIDTH + (COL_WIDTH + COL_DIST) * i, colHeight, {name: names[i], score: times[i]}, colColor);
   }
 };
 
-var getBiggestScore = function (players, scores) {
+var getBiggestScore = function (scores) {
   var maxScore = 0;
-  var playerName = '';
   for (var i = 0; i < scores.length; i++) {
-    if (scores[i] > maxScore) {
-      maxScore = scores[i];
-      playerName = players[i];
-    }
+    maxScore = (scores[i] > maxScore) ? scores[i] : maxScore;
   }
-  return {name: playerName, score: Math.floor(maxScore)};
+  return Math.floor(maxScore);
+};
+
+var getColHeight = function (times, checkedScore) {
+  var biggestScore = getBiggestScore(times);
+  return (checkedScore === biggestScore) ? HISTM_HEIGHT : HISTM_HEIGHT * (checkedScore / biggestScore);
+};
+
+var getColColor = function (curPlayer) {
+  var userColor = 'rgba(255, 0, 0, 1)';
+  var otherColor = 'rgba(0, 0, 255, ' + Math.random() + ')';
+  return (curPlayer === 'Вы') ? userColor : otherColor;
 };
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_SHADOW_COL);
-   renderCloud(ctx, CLOUD_X, CLOUD_Y);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderText(ctx, CLOUD_X + 2 * GAP, CLOUD_Y + 3 * GAP, 'Ура вы победили!');
+  renderText(ctx, CLOUD_X + 2 * GAP, CLOUD_Y + 3 * GAP + FONT_SIZE, 'Список результатов:');
 
-   renderText(ctx, CLOUD_X + 2 * GAP, CLOUD_Y + 3 * GAP, 'Ура вы победили!');
-   renderText(ctx, CLOUD_X + 2 * GAP, CLOUD_Y + 3 * GAP + FONT_SIZE, 'Список результатов:');
-
-   renderAllCols(ctx, names, times);
+  renderAllCols(ctx, names, times);
 };
